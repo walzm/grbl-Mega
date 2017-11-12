56317b2a0759286fe3739b904ee69868163bee1c
Mathias Walz <mathias.walz@sage.de>
2017-11-12 10:01:05 +0100

    Build version info

--- grbl/grbl.h
+++ grbl/grbl.h
@@ -23,7 +23,7 @@
 
 // Grbl versioning system
 #define GRBL_VERSION "1.1f"
-#define GRBL_VERSION_BUILD "20170802"
+#define GRBL_VERSION_BUILD "2017-11-12-e6cb0e3"
 
 // Define standard libraries used by Grbl.
 #include <avr/io.h>
